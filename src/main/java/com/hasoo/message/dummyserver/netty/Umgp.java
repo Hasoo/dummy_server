a0be8604168a5914a042cf44dda1a624381a671c
Hasoo <vaxzeem@i-heart.co.kr>
2019-01-14 18:31:14 +0900

    define HEADER, DATA PART

---
+++ src/main/java/com/hasoo/message/dummyserver/netty/Umgp.java
@@ -0,0 +1,33 @@
+package com.hasoo.message.dummyserver.netty;
+
+public class Umgp {
+  /* @formatter:off */
+  public static final String BEGIN          = "BEGIN";
+  public static final String CONNECT        = "CONNECT";
+  public static final String SEND           = "SEND";
+  public static final String MMS            = "MMS";
+  public static final String REPORT         = "REPORT";
+  public static final String PING           = "PING";
+  public static final String PONG           = "PONG";
+  public static final String ACK            = "ACK";
+  public static final String AUTH           = "AUTH";
+  public static final String END            = "END";
+  
+  public static final String ID             = "ID";
+  public static final String PASSWORD       = "PASSWORD";
+  public static final String REPORTLINE     = "REPORTLINE";
+  public static final String VERSION        = "VERSION";
+  public static final String SUBJECT        = "SUBJECT";
+  public static final String RECEIVERNUM    = "RECEIVERNUM";
+  public static final String CALLBACK       = "CALLBACK";
+  public static final String CODE           = "CODE";
+  public static final String DATA           = "DATA";
+  public static final String KEY            = "KEY";
+  public static final String DATE           = "DATE";
+  public static final String NET            = "NET";
+  public static final String IMAGE          = "IMAGE";
+  public static final String CONTENTTYPE    = "CONTENTTYPE";
+  public static final String AGENT          = "AGENT";
+  public static final String TTL            = "TTL";
+  /* @formatter:on */
+}
