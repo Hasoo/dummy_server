c2752cbe57c6aa60641ca4f5e9de57f7e88538b4
Hasoo <vaxzeem@i-heart.co.kr>
2019-01-18 18:33:56 +0900

    Filesequence benchmark

--- src/test/java/com/hasoo/message/dummyserver/FileSequenceTest.java
+++ src/test/java/com/hasoo/message/dummyserver/FileSequenceTest.java
@@ -19,6 +19,10 @@ public class FileSequenceTest {
         Files.delete(p);
       }
 
+      /*
+       * , benchmark(1,000,000) opened:117,647 reopen:153,846 reopen lock:222,222
+       */
+
       // int maxValue = 1_000;
       int maxValue = 1_000_000;
       FileSequence fileSequence = new FileSequence(path, filename, maxValue);
